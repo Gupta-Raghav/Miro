6f81690585ef70dbd56b7b2147d778c077abd843
Gupta-Raghav <42888633+Gupta-Raghav@users.noreply.github.com>
2024-02-07 12:25:57 -0500

    loading screen, Authentication using Clerk, setup Convex(Backend).

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://profound-starling-66.clerk.accounts.dev", //this comes from the issues field of the JWT from the clerk 
+        applicationID: "convex",
+      },
+    ]
+  };
